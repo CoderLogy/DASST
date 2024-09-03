e1f0d2708e76482c7ae28c5119e18eb3b7aae42b
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2024-09-03 00:18:48 -0700

    additions

---
+++ setup.py
@@ -0,0 +1,21 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name="disk_usage",  # The name of your package
+    version="0.1",  # The initial release version
+    packages=find_packages(),  # Automatically find package directories
+    description="A Python package to read disk usage",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Shourya Wadhwa",
+    author_email="sourceboxtv@gmail.com",
+    url="https://github.com/CoderLogy/Python-Disk-Usage",  
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
