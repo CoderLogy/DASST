1cc792273ecf7e40e0e3af29f6dfc72842f1c75d
Shourya wadhwa <71631493+CoderLogy@users.noreply.github.com>
2024-09-03 00:56:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,14 @@ setup(
     url="https://github.com/CoderLogy/Python-Disk-Usage",  
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL version 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    install_requires=[
+        "psutil","resource"
+    ],
+    include_package_data=True,
+    license="GPL-3.0-or-later",
+    license_files=('LICENSE',),
 )
